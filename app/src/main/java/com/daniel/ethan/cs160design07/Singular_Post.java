59e73ae0cfbec821e1cc27b2948fbf32db1cdfcf
EitherCod <vchadala@berkeley.edu>
2022-04-28 17:33:08 -0700

    Smalled Changes to Singular Post + Trying out Search View

--- app/src/main/java/com/daniel/ethan/cs160design07/Singular_Post.java
+++ app/src/main/java/com/daniel/ethan/cs160design07/Singular_Post.java
@@ -10,5 +10,8 @@ public class Singular_Post extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singular_post);
+
     }
+
+
 }
