a7876b0c48ecb9ba0ca597f70508aa2bdcd2cbfb
EitherCod <vchadala@berkeley.edu>
2022-04-28 18:46:58 -0700

    Add nav bar to singular post

--- app/src/main/java/com/daniel/ethan/cs160design07/Singular_Post.java
+++ app/src/main/java/com/daniel/ethan/cs160design07/Singular_Post.java
@@ -2,7 +2,9 @@ package com.daniel.ethan.cs160design07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Singular_Post extends AppCompatActivity {
 
@@ -11,6 +13,11 @@ public class Singular_Post extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singular_post);
 
+    }
+
+    public void toHome(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
 
