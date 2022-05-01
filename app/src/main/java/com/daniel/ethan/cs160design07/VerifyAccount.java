93872565e98749f8ea6820767318452e119a6f41
Ryan Adami <ryanadami@berkeley.edu>
2022-04-30 18:21:39 -0700

    verify page

---
+++ app/src/main/java/com/daniel/ethan/cs160design07/VerifyAccount.java
@@ -0,0 +1,14 @@
+package com.daniel.ethan.cs160design07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_account);
+    }
+}
