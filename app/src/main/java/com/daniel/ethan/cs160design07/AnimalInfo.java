4781cd3e5de3c7ab1796f17ba3d65a97900b0f41
Juliet Kim <suhyang@berkeley.edu>
2022-04-30 22:57:56 -0700

    info pg and supplies checklist

---
+++ app/src/main/java/com/daniel/ethan/cs160design07/AnimalInfo.java
@@ -0,0 +1,14 @@
+package com.daniel.ethan.cs160design07;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimalInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_info);
+    }
+}
