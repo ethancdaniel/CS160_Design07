b3702bd9c557d81aeedcdae929becfc69e6c7555
Ryan Adami <ryanadami@berkeley.edu>
2022-04-30 19:48:28 -0700

    login

--- app/src/main/java/com/daniel/ethan/cs160design07/CreateAccount.java
+++ app/src/main/java/com/daniel/ethan/cs160design07/CreateAccount.java
@@ -2,7 +2,10 @@ package com.daniel.ethan.cs160design07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+    }
+
+    public void toCreateAccount(View view) {
+        CheckBox checkbox1 = (CheckBox)findViewById(R.id.checkBox);
+        if (!checkbox1.isChecked()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, VerifyAccount.class);
+            startActivity(intent);
+        }
     }
 }
